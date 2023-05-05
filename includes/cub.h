d7174523f5dd956d9d45c6a5c20220804d13402d
Guilhem Raux <graux@c1r6s1.42lausanne.ch>
2023-05-05 11:54:45 +0200

    structs to store program data

--- includes/cub.h
+++ includes/cub.h
@@ -6,12 +6,15 @@
 /*   By: graux <graux@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 11:06:34 by graux             #+#    #+#             */
-/*   Updated: 2023/05/05 11:07:06 by graux            ###   ########.fr       */
+/*   Updated: 2023/05/05 11:54:20 by graux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB_H
 # define CUB_H
+
+# define WIN_W 1280
+# define WIN_H 720
 
 typedef struct s_frame
 {
@@ -21,5 +24,48 @@ typedef struct s_frame
 	int		line_length;
 	int		endian;
 }				t_frame;
+
+enum e_map_dir
+{
+	NO,
+	SO,
+	WE,
+	EA
+};
+
+typedef struct s_color
+{
+	int	r;
+	int	g;
+	int	b;
+}			t_color;
+
+typedef struct s_vec2f
+{
+	float	x;
+	float	y;
+}			t_vec2f;
+
+typedef struct s_player
+{
+	t_vec2f	pos;
+	t_vec2f	dir;
+}			t_player;
+
+typedef struct s_map
+{
+	t_color		f_color;
+	t_color		c_color;
+	t_frame		textures[4];
+	t_player	player;
+}			t_map;
+
+typedef struct s_gui
+{
+	void	*mlx;
+	void	*mlx_win;
+}			t_gui;
+
+t_map	*map_load(char *map_path);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,15 +6,21 @@
 /*   By: graux <graux@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 11:08:52 by graux             #+#    #+#             */
-/*   Updated: 2023/05/05 11:09:44 by graux            ###   ########.fr       */
+/*   Updated: 2023/05/05 11:32:23 by graux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub.h"
+#include "../includes/mlx.h"
 
 int	main(int argc, char *argv[])
 {
+	t_gui	gui;
+
 	(void) argc;
 	(void) argv;
+	gui.mlx = mlx_init();
+	gui.mlx_win = mlx_new_window(gui.mlx, WIN_W, WIN_H, "cub3D");
+	mlx_loop(gui.mlx);
 	return (0);
 }
