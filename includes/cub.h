8c9e5aaa9bb76756f884c2262e30cbb21d52ee56
Guilhem Raux <graux@c1r6s1.42lausanne.ch>
2023-05-09 17:04:10 +0200

    changed header a bit

--- includes/cub.h
+++ includes/cub.h
@@ -6,7 +6,7 @@
 /*   By: graux <graux@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 11:06:34 by graux             #+#    #+#             */
-/*   Updated: 2023/05/05 11:54:20 by graux            ###   ########.fr       */
+/*   Updated: 2023/05/09 17:03:14 by graux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 # define WIN_W 1280
 # define WIN_H 720
+
+# define FOV 60 //TODO tranform to rad
 
 typedef struct s_frame
 {
@@ -46,10 +48,27 @@ typedef struct s_vec2f
 	float	y;
 }			t_vec2f;
 
+typedef struct s_vec2i
+{
+	int	x;
+	int	y;
+}			t_vec2i;
+
+typedef struct s_mat2x2
+{
+	float	elem1_1;
+	float	elem1_2;
+	float	elem2_1;
+	float	elem2_2;
+}			t_mat2x2;
+
 typedef struct s_player
 {
-	t_vec2f	pos;
-	t_vec2f	dir;
+	t_vec2f		pos;	 // (5 * 32, 7 * 32)
+	t_vec2i		pos_map; // (5, 7)
+	t_vec2f		dir;
+	t_mat2x2	m_rotations[WIN_W];
+	t_vec2f		rays[WIN_W]; //TODO change to something smaller?
 }			t_player;
 
 typedef struct s_map
@@ -58,14 +77,16 @@ typedef struct s_map
 	t_color		c_color;
 	t_frame		textures[4];
 	t_player	player;
+	t_vec2i		map_size;
 }			t_map;
 
 typedef struct s_gui
 {
 	void	*mlx;
 	void	*mlx_win;
+	t_frame	*screen;
 }			t_gui;
 
-t_map	*map_load(char *map_path);
+int	map_load(t_map *map, char *map_path);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: graux <graux@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 11:08:52 by graux             #+#    #+#             */
-/*   Updated: 2023/05/05 11:32:23 by graux            ###   ########.fr       */
+/*   Updated: 2023/05/09 17:01:53 by graux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,13 @@
 int	main(int argc, char *argv[])
 {
 	t_gui	gui;
+	t_map	map;
 
 	(void) argc;
 	(void) argv;
+	map_load(&map, argv[1]);
+	map.player.dir.x = 1;
+	map.player.dir.y = 0;
 	gui.mlx = mlx_init();
 	gui.mlx_win = mlx_new_window(gui.mlx, WIN_W, WIN_H, "cub3D");
 	mlx_loop(gui.mlx);
